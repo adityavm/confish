745e9c7c41ebb72eeedd46b5a82db0422edd1381
Aditya Mukherjee <aditya@adityamukherjee.com>
2020-09-16 05:46:14 +1000

    Better ubersicht updating, add keybinding for dnd

--- dnd.fish
+++ dnd.fish
@@ -1,2 +1,25 @@
-# Requires https://github.com/sindresorhus/do-not-disturb-cli
-alias dnd "do-not-disturb"
+function is_dnd_enabled
+  set -l dndstatus (defaults -currentHost read ~/Library/Preferences/ByHost/com.apple.notificationcenterui doNotDisturb -boolean)
+  if [ $dndstatus = '1' ]
+    echo "true"
+  else
+    echo "false"
+  end
+end
+
+function enable_dnd
+  defaults -currentHost write ~/Library/Preferences/ByHost/com.apple.notificationcenterui doNotDisturb -boolean true
+end
+
+function disable_dnd
+  defaults -currentHost write ~/Library/Preferences/ByHost/com.apple.notificationcenterui doNotDisturb -boolean false
+end
+
+function toggle_dnd
+  set -l isdnd (is_dnd_enabled)
+  if [ $isdnd = 'true' ]
+    disable_dnd
+  else
+    enable_dnd
+  end
+end
